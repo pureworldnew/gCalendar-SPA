97c51064602c35d1b40845db8275ca9bdd4e1a6c
Pure World <pureworldnew@gmail.com>
2022-06-23 10:21:36 -0500

    inital google calendar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import GoogleCalendar from "./components/GoogleCalendar";
+
+function App() {
+  return (
+    <div className="App">
+      <GoogleCalendar />
+    </div>
+  );
+}
+
+export default App;
